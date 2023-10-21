a29461cdf6c87a6a65c1a96693c6b43a4b084f79
Davlatbek Kobiljonov Sherzodovich <117518580+Davlatbek0710@users.noreply.github.com>
2023-10-21 21:12:53 +0500

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -36,31 +36,33 @@ all_combinations = [rock, paper, scissors]
 
 print("What do you choose?, enter 0 for rock, 1 for paper, 2 for scissors ", end="")
 user_choice = int(input())
-
-print("Your choice:")
-
-print(all_combinations[user_choice])
-
-computer_choice = random.randint(0, 2)
-
-print("Computer's choice: ")
-
-print(all_combinations[computer_choice])
-
-# 0 = rock
-# 1 = paper
-# 2 = scissors
-
-if user_choice == computer_choice:
-    print("Draw")
-elif user_choice == 0 and computer_choice == 2:
-    print("You won!")
-elif user_choice == 1 and computer_choice == 0:
-    print("You win!")
-elif user_choice == 2 and computer_choice == 1:
-    print("You win!")
+if (user_choice >= 3 or user_choice<0):
+    print("You entered invalid number, you lose!")
 else:
-    print("Computer wins!")
+    print("Your choice:")
+    
+    print(all_combinations[user_choice])
+    
+    computer_choice = random.randint(0, 2)
+    
+    print("Computer's choice: ")
+    
+    print(all_combinations[computer_choice])
+    
+    # 0 = rock
+    # 1 = paper
+    # 2 = scissors
+    
+    if user_choice == computer_choice:
+        print("Draw")
+    elif user_choice == 0 and computer_choice == 2:
+        print("You won!")
+    elif user_choice == 1 and computer_choice == 0:
+        print("You win!")
+    elif user_choice == 2 and computer_choice == 1:
+        print("You win!")
+    else:
+        print("Computer wins!")
 
 
 
