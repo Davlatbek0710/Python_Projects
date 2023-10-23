86e83810f66d8f9c405082f6ecffeb4626e250db
Davlatbek Kobiljonov Sherzodovich <117518580+Davlatbek0710@users.noreply.github.com>
2023-10-23 09:10:26 +0500

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -9,7 +9,7 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '.', ',', '~']
 
 print("Welcome to password generator!")
 number_of_letters = int(input("How many letter you want to have in your password? "))
